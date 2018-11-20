577f85d4a227b5b2cca0588fcd7d5dba5ce15fd1
zzskm <rennintherynn@gmail.com>
2018-11-20 18:16:21 +0900

    improve visualization

--- Program.cs
+++ Program.cs
@@ -10,13 +10,28 @@ namespace Maze_Gen
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
             Console.WriteLine("Select one of the algorithms:");
+            Console.WriteLine();
+
             Console.WriteLine("1. Hunt-and-Kill");
             Console.WriteLine("2. Recursive Backtracking");
             Console.WriteLine("3. Prim's Algorithm");
             Console.WriteLine("4. Kruskal's Algorithm");
 
+            Console.WriteLine();
             Console.Write("> Enter your selection (1-4): ");
+
             int sel = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("> Width : ");
+            int w = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("> Height : ");
+            int h = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("> Fps : ");
+            //int fps = Convert.ToInt32(Console.ReadLine());
+            //int interval = 1000 / fps;
+            int interval = 1000 / 360;
 
             Maze mz = new Maze();
 
@@ -32,22 +47,6 @@ namespace Maze_Gen
                     return;
             }
 
-            int w, h;
-
-            Console.Write("> Width : ");
-            w = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("> Height : ");
-            h = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("> Fps : ");
-
-            //int fps = Convert.ToInt32(Console.ReadLine());
-            //int interval = 1000 / fps;
-            int interval = 1000 / 120;
-
-            Console.CursorVisible = false;
-
             int screenWidth = w * 2 + 5;
             int screenHeight = h * 2 + 5;
 
@@ -60,6 +59,8 @@ namespace Maze_Gen
             {
                 Console.WindowHeight = screenHeight;
             }
+
+            Console.CursorVisible = false;
 
             Console.Clear();
             Console.WriteLine(mz.m_gen.GetType().Name + " [" + w + ", " + h + "]");
